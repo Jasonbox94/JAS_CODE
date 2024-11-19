82a153576479e2a372ee997683b1fa7761d3d790
Jasonbox94 <hsin_yo_lin@msn.com>
2024-11-19 04:05:54 +0000

    100000

--- open_file.py
+++ open_file.py
@@ -1,6 +1,10 @@
 #open file
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for row in f:
+        count += 1
         data.append(row)
-print(len(data))
+        if count % 100000 == 0: 
+            print(len(data))
+            print(row)
